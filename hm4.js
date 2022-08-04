320e1891cd805b8435f6c1defd3d2ffbc71bfa6e
Nepryash <nepryash05@gmail.com>
2022-08-04 19:54:13 +0200

    homeworkJS

---
+++ hm4.js
@@ -0,0 +1,57 @@
+//1
+let obj1 = {
+    name: 'Ivan',
+    age:23,
+}
+let obj2 = {
+    occupation: 'Builder'
+}
+// let inGen = {...obj1,...obj2}
+// console.log(inGen)
+// //2
+// let task2 = [1,2,3]
+// task2.push('a','b','c')
+// let splice = task2.splice(6,0,'a','b','c')
+// console.log(task2)
+// //3
+// let task3 = [1,2,3,4,5]
+// task3.splice(1,2)
+// console.log(task3)
+// //4
+// let task4 = [1, 2, 3, 4, 5] 
+// let splice4 = task4.splice(3,0,'a','b','c')
+// console.log(task4)
+////5
+ const arr = [1, 2, 3, 4, 5]
+ let multiply = 10;
+ arr.array.forEach(element => {
+    
+ }); =>)
+//6
+// for (let i = 2; i < 400; i++) {
+//     const el =[i];
+//     console.log(el)
+// };
+//7
+// for (let i = 4; i < 15; i+=3) {
+//     const element = [i];
+//     console.log(element)
+    
+// }
+//8
+//  for (let i = 654; i > 0; i--) {
+//      const element = [i];
+//      console.log(element)
+//  }
+//9
+// for (let i = 1983; i <= 2017; i++) {
+//     const element = [i];
+//     console.log(element)
+// }
+//10
+
+// for (let i = -4; i <= 100; i+=2) {
+//     const element = [i];
+//     console.log(element)
+// }
+//
